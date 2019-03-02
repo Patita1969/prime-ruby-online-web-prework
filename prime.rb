23765c6002e7dd8271aaf73f2b783d32f2ce08c0
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-02 08:04:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 # Add  code here
-        def prime n
-          2.upto(Math.sqrt(n)) do |x|
-            if n % x == 0
-              the square root of the number
-              return false
-            else
-              return true
+def prime n
+  2.upto(Math.sqrt(n)) do |x|
+  if n % x == 0
+  the square root of the number
+    return false
+      else
+    return true
             end
           end
         end
