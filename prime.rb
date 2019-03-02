c7e199cfc62548da554baf820ef8a9e93ae672d8
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-02 08:11:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here
-def is_prime(n)
-  if (n < 2)
+def prime?(n)
+  if n < 2
     return false
   end
-  if (n == 2)
+  if n == 2
     return true
   end
-  if (n%2 == 0)
+  if n%2 == 0
     return false
   end
   d = 3
